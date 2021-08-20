1addf7d8d412429c159e0f890c6bccf32118b399
Nash_Traineee <55914156+TraineeNash@users.noreply.github.com>
2021-08-20 11:28:16 +0800

    adding template css

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.querySelector("#btn").addEventListener("click", function(){
+    document.querySelector(".num").classList.add("show")
+    // alert(document.querySelector(".num").classList)
+})
